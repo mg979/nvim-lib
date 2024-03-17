d9b0331c129126afe1a8fffa556f8201f1575dea
Gianmaria Bajo <mg1979.git@gmail.com>
2024-03-17 04:21:42 +0100

    refactor: nvim.keycodes

--- lua/nvim-lib/nvim/keycodes.lua
+++ lua/nvim-lib/nvim/keycodes.lua
@@ -1,3 +1,5 @@
+local termkeys = vim.api.nvim_replace_termcodes
+
 -------------------------------------------------------------------------------
 -- Keycodes as they can be used in lua scripts.
 -- Keys like Esc (instead of <Esc>), CtrlO (instead of <C-o>) are also valid.
@@ -5,8 +7,10 @@ return setmetatable({}, {
   __metatable = false,
   __index = function(t, k)
     local K = k
-    if k:find('^<%w+>$') then
-      k = k:gsub('[<>]', '') -- lowercase k, not a mistake
+    if k:find('^<[a-zA-Z]+>$') then
+      -- lowercase k, not a mistake
+      -- so that <cr>, <tab>, etc. will be also found as cr, tab, etc.
+      k = k:gsub('[<>]', '')
     elseif k:find('^Ctrl.+') then
       K = '<C-' .. k:sub(5) .. '>'
     elseif k:find('^Shift.+') then
@@ -18,7 +22,7 @@ return setmetatable({}, {
     elseif k:find('^%w+$') then
       K = '<' .. k .. '>'
     end
-    local ok, key = pcall(vim.api.nvim_replace_termcodes, K, true, true, true)
+    local ok, key = pcall(termkeys, K, true, true, true)
     -- replace_termcodes returns the unchanged string if it's not a keycode
     if ok and key ~= K then
       -- set both forms (keycode/stringified)
